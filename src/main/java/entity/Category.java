40d2b8fa8192cd9b8a5d05a3c9338fb3980a8976
Luka Zečević <zecevicluka10@gmail.com>
2025-08-21 02:13:46 +0200

    dodati modeli

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -1,5 +1,69 @@
 package entity;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Category {
+@Entity
+@Table(name ="CATEGORIES")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)   // Ili prihod ili trosak
+    @Column(name = "TYPE", nullable = false)
+    private CategoryType type;
+
+    @Column(name = "PREDEFINED", nullable = false)
+    private boolean predefined;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = true)
+    private User user;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Transaction> transactions = new HashSet<>();
+
+    public Category() {}
+
+    public Category(String name, CategoryType type, boolean predefined, User user) {
+        this.name = name;
+        this.type = type;
+        this.predefined = predefined;
+        this.user = user
+        ;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public CategoryType getType() { return type; }
+    public void setType(CategoryType type) { this.type = type; }
+
+    public boolean isPredefined() { return predefined; }
+    public void setPredefined(boolean predefined) { this.predefined = predefined; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public Set<Transaction> getTransactions() { return transactions; }
+    public void setTransactions(Set<Transaction> transactions) { this.transactions = transactions; }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id
+                + ", name=" + name
+                + ", type=" + type
+                + ", predefined=" + predefined //
+                + ", user=" + (user != null ? user.getId() : "null")
+                + ", transactions=" + transactions.size()
+                + "]";
+    }
+
 
 }
--- src/main/java/entity/Transaction.java
+++ src/main/java/entity/Transaction.java
@@ -1,5 +1,92 @@
 package entity;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Transaction {
+@Entity
+@Table(name = "TRANSACTIONS")
+public class Transaction implements Serializable {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "AMOUNT", nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private CategoryType type;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "RECURRING", nullable = false)
+    private boolean recurring;
+
+    @Column(name = "FREQUENCY", nullable = true)
+    private String frequency;
+
+    @Column(nullable = false)
+    private LocalDate transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public CategoryType getType() { return type; }
+    public void setType(CategoryType type) { this.type = type; }
+
+    public Category getCategory() { return category; }
+    public void setCategory(Category category) { this.category = category; }
+
+    public boolean isRecurring() { return recurring; }
+    public void setRecurring(boolean recurring) { this.recurring = recurring; }
+
+    public String getFrequency() { return frequency; }
+    public void setFrequency(String frequency) { this.frequency = frequency; }
+
+    public LocalDate getTransactionDate() { return transactionDate; }
+    public void setTransactionDate(LocalDate transactionDate) { this.transactionDate = transactionDate; }
+
+    public Wallet getWallet() { return wallet; }
+    public void setWallet(Wallet wallet) { this.wallet = wallet; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    @Override
+    public String toString() {
+        return "Transaction [id=" + id
+                + ", name=" + name
+                + ", amount=" + amount
+                + ", type=" + type
+                + ", category=" + (category != null ? category.getId() : "null")
+                + ", recurring=" + recurring
+                + ", frequency=" + frequency
+                + ", transactionDate=" + transactionDate
+                + ", wallet=" + (wallet != null ? wallet.getId() : "null")
+                + ", user=" + (user != null ? user.getId() : "null")
+                + "]";
+    }
+
+
 
 }
