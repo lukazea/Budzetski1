f925135bedbe04a4674a1821de89dc2ef6c68776
Luka Zečević <zecevicluka10@gmail.com>
2025-09-10 01:06:19 +0200

    Prva funkcionalnost

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,25 @@
 package controller;
 
+import dto.UserDto;
+import service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    // Javno dostupan endpoint za broj registrovanih korisnika (prva funkcionalnost)
+    @GetMapping("/public/count")
+    public ResponseEntity<UserDto> getTotalUserCount() {
+        long totalUsers = userService.getTotalUserCount();
+        UserDto response = new UserDto();
+        response.setTotalUserCount(totalUsers);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,19 @@
 package service;
 
+import repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    // Prva funkcionalnost - broj ukupnih registrovanih korisnika
+    public long getTotalUserCount() {
+        return userRepository.count();
+    }
 }
