8b495f7b71cdf73d6861a5c68b5cd378c1b63342
Irina Radulović <irinaradulovic04@gmail.com>
2025-09-10 09:08:15 -0400

    Kompletne klase za valutu

--- src/main/java/dto/CurrencyDto.java
+++ src/main/java/dto/CurrencyDto.java
@@ -1,5 +1,28 @@
 package dto;
 
-public class CurrencyDto {
+import entity.Currency;
 
+public class CurrencyDto {
+    private Long id;
+    private String currency;
+    private float valueToEur;
+
+    public CurrencyDto() {}
+
+    public CurrencyDto(Currency currency) {
+        this.id = currency.getId();
+        this.currency = currency.getCurrency();
+        this.valueToEur = currency.getValueToEur();
+    }
+
+    // Getteri i setteri
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    
+    public String getCurrency() { return currency; }
+    public void setCurrency(String currency) { this.currency = currency; }
+    
+    public float getValueToEur() { return valueToEur; }
+    public void setValueToEur(float valueToEur) { this.valueToEur = valueToEur; }
 }
+
