61d67685e20193aae3ff4a2c08816c24b330dbbd
Irina Radulović <irinaradulovic04@gmail.com>
2025-09-11 13:19:54 -0400

    Jos jedna klasa dodata

--- src/main/java/dto/GoalProgressDTO.java
+++ src/main/java/dto/GoalProgressDTO.java
@@ -1,5 +1,14 @@
 package dto;
 
-public class GoalProgressDTO {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+public class GoalProgressDTO {
+    private Long goalId;
+    private String goalName;
+    private BigDecimal targetAmount;
+    private BigDecimal currentAmount;
+    private BigDecimal progressPercentage;
+    private LocalDate deadline;
+    private long daysRemaining;
 }
