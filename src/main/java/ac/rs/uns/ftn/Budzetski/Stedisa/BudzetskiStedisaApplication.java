4450c15d42fb01ada013c540d4c4c75b36acbf0b
Irina Radulović <irinaradulovic04@gmail.com>
2025-09-25 03:27:01 -0400

    Paketi su sad deo Aplication.java

--- src/main/java/ac/rs/uns/ftn/Budzetski/Stedisa/BudzetskiStedisaApplication.java
+++ src/main/java/ac/rs/uns/ftn/Budzetski/Stedisa/BudzetskiStedisaApplication.java
@@ -2,8 +2,24 @@ package ac.rs.uns.ftn.Budzetski.Stedisa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+	    "ac.rs.uns.ftn.Budzetski.Stedisa", 
+	    "controller",                        
+	    "service",                          
+	    "repository",                        
+	    "dto",
+	    "entity"
+	})
+
+@EnableJpaRepositories(basePackages = {"repository"}) 
+@EntityScan(basePackages = {"entity"})              
+
 public class BudzetskiStedisaApplication {
 
 	public static void main(String[] args) {
